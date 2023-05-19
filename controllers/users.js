3c9a0231e8476493079a618777bb9337f24f3c3d
LostLord07 <satishsingh45264209@gmail.com>
2023-05-19 11:30:33 +0530

    changed condition for adding friends

--- controllers/users.js
+++ controllers/users.js
@@ -33,13 +33,14 @@ export const getUserFriends = async (req, res) => {
 /* UPDATE */
 export const addRemoveFriend = async (req, res) => {
   try {
+
     const { id, friendId } = req.params;
     const user = await User.findById(id);
     const friend = await User.findById(friendId);
 
-    if (user.friends.includes(friendId)) {
+    if (user.friends.includes(friendId) && id != friendId) {
       user.friends = user.friends.filter((id) => id !== friendId);
-      friend.friends = friend.friends.filter((id) => id !== id);
+      friend.friends = friend.friends.filter((iid) => iid !== id);
     } else {
       user.friends.push(friendId);
       friend.friends.push(id);
